4dc9b5ecf0bc32fbb9cbaf42184eceedd14afa95
Rockjai1213 <104734341+Rockjai1213@users.noreply.github.com>
2022-05-15 12:29:02 +0100

    Add files via upload

--- Config.lua
+++ Config.lua
@@ -1,10 +1,10 @@
 Config = {}
 Config.ESX = true
-Config.BotToken = "OTcxMDk3NTg3OTUwMTc4MzU0.G6D748.T938-s2b4gT6X8T1IfE3ICTq_4NjWYJ9R47KeM"
-Config.WebHook = "https://discord.com/api/webhooks/975335083504467998/T2C_fKTTxGDQu9FXwPKmz4a4rE-vOPDGADHlWd4atZN31wobxBLdQpu7pd-R-ZG3gEAZ"
-Config.ChannelID = "971096015291699250"
-Config.ReplyUserName = "Server Control"
-Config.AvatarURL = "https://cdn.discordapp.com/attachments/964703038507978772/964828410578874388/logo.png"
+Config.BotToken = ""
+Config.WebHook = ""
+Config.ChannelID = ""
+Config.ReplyUserName = ""
+Config.AvatarURL = ""
 Config.Prefix = "!"
 Config.WaitEveryTick = 4000  -- ITS MS
 
